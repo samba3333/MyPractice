4fef1211d8611a4c74edf416f48ee0c779c5961a
Sambasiva-TC <sambasiva.gandru@toucanus.com>
2025-01-22 12:24:30 +0530

    Spring Reactive microservie practice

---
+++ reactive-microservice/src/main/java/com/samba/reactive_microservice/ReactiveMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.samba.reactive_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMicroserviceApplication.class, args);
+	}
+
+}
---
+++ reactive-microservice/src/test/java/com/samba/reactive_microservice/ReactiveMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samba.reactive_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
